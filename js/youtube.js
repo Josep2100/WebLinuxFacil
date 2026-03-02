7ea7f2536e2842956c6ae94505e4d4c88f6a434c
Josep2100 <josepcalvoasix@gmail.com>
2026-03-02 19:26:14 +0100

    Refactor YouTube API fetch to use CHANNEL_ID

--- js/youtube.js
+++ js/youtube.js
@@ -1,35 +1,33 @@
 const API_KEY = "AIzaSyCTqXue0jpubIfHlRMkryJLgYDJx_KD8GI";
-const CHANNEL_USERNAME = "josepcalvoasix";
+const CHANNEL_ID = "UC5LxrchV-nNK5gYJa0Ipsbw";
 
-fetch(`https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=${CHANNEL_USERNAME}&key=${API_KEY}`)
-  .then(res => res.json())
-  .then(data => {
+const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&maxResults=50&order=date&type=video&key=${API_KEY}`;
 
-    const uploadsPlaylistId = data.items[0].contentDetails.relatedPlaylists.uploads;
-
-    return fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${uploadsPlaylistId}&key=${API_KEY}`);
-  })
-  .then(res => res.json())
+fetch(URL)
+  .then(response => response.json())
   .then(data => {
 
     const container = document.getElementById("video-container");
 
-    data.items.forEach(item => {
+    if (!data.items || data.items.length === 0) {
+        container.innerHTML = "<p style='text-align:center;'>No se encontraron videos.</p>";
+        return;
+    }
 
-      const videoId = item.snippet.resourceId.videoId;
+    data.items.forEach(video => {
 
-      container.innerHTML += `
-        <div class="video-card">
-          <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
-            <img src="${item.snippet.thumbnails.high.url}">
-            <div class="title">${item.snippet.title}</div>
-          </a>
-        </div>
-      `;
+        container.innerHTML += `
+            <div class="video-card">
+                <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank">
+                    <img src="${video.snippet.thumbnails.high.url}">
+                    <div class="title">${video.snippet.title}</div>
+                </a>
+            </div>
+        `;
     });
 
   })
   .catch(error => {
-    document.getElementById("video-container").innerHTML =
-    "<p style='text-align:center;'>No se pudieron cargar los videos.</p>";
+      document.getElementById("video-container").innerHTML =
+      "<p style='text-align:center;'>Error al conectar con la API.</p>";
   });
